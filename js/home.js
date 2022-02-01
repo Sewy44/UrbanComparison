9526e9781df6e5bcb4823c5a65c13aeff5a8422b
Sewy44 <76139710+Sewy44@users.noreply.github.com>
2022-01-31 21:04:38 -0500

    MVP commit

--- js/home.js
+++ js/home.js
@@ -3,28 +3,59 @@ $(document).ready(function () {
 });
 
 function compare() {
+    $('#errorMessages').empty();
+    $('#search1Table').empty();
+    $('#search2Table').empty();
     searchFirstUA();
     searchSecondUA();
 }
 
 function searchFirstUA() {
+
     var firstSearchParam = $('#firstUASearch').val();
+    var searchOne = $('#search1Table');
 
     $.ajax({
         type: "GET",
         url: "https://api.teleport.org/api/urban_areas/slug:" + firstSearchParam + "/scores/",
-        success: function (data) {
-            var urban1
+        success: function (dataArray) {
+            var content = '<tr><td>' + dataArray.categories[1].name + '</td><td>' + dataArray.categories[1].score_out_of_10.toFixed(2) + '</td></tr>' +
+                '<tr><td>' + dataArray.categories[7].name + '</td><td>' + dataArray.categories[7].score_out_of_10.toFixed(2) + '</td></tr>' +
+                '<tr><td>' + dataArray.categories[8].name + '</td><td>' + dataArray.categories[8].score_out_of_10.toFixed(2) + '</td></tr>' +
+                '<tr><td>' + dataArray.categories[10].name + '</td><td>' + dataArray.categories[10].score_out_of_10.toFixed(2) + '</td></tr>' +
+                '<tr><td>' + dataArray.categories[15].name + '</td><td>' + dataArray.categories[15].score_out_of_10.toFixed(2) + '</td></tr>'
+
+            searchOne.append(content);
+        }, error: function () {
+            $('#errorMessages')
+                .append($('<li>')
+                    .attr({ class: 'list-group-item list-group-item-danger' })
+                    .text('Error calling web service. Please try again later.'));
         }
     })
 }
 
 function searchSecondUA() {
-    var secondSearchParam = $('#secondUASearch').val();
+    var firstSearchParam = $('#secondUASearch').val();
+    var searchOne = $('#search2Table');
 
     $.ajax({
         type: "GET",
-        url: "https://api.teleport.org/api/urban_areas/slug:" + secondSearchParam + "/scores/",
-        success: 
+        url: "https://api.teleport.org/api/urban_areas/slug:" + firstSearchParam + "/scores/",
+        success: function (dataArray) {
+            var content = '<tr><td>' + dataArray.categories[1].name + '</td><td>' + dataArray.categories[1].score_out_of_10.toFixed(2) + '</td></tr>' +
+                '<tr><td>' + dataArray.categories[7].name + '</td><td>' + dataArray.categories[7].score_out_of_10.toFixed(2) + '</td></tr>' +
+                '<tr><td>' + dataArray.categories[8].name + '</td><td>' + dataArray.categories[8].score_out_of_10.toFixed(2) + '</td></tr>' +
+                '<tr><td>' + dataArray.categories[10].name + '</td><td>' + dataArray.categories[10].score_out_of_10.toFixed(2) + '</td></tr>' +
+                '<tr><td>' + dataArray.categories[15].name + '</td><td>' + dataArray.categories[15].score_out_of_10.toFixed(2) + '</td></tr>'
+
+            searchOne.append(content);
+        }, error: function () {
+            $('#errorMessages')
+                .append($('<li>')
+                    .attr({ class: 'list-group-item list-group-item-danger' })
+                    .text('Error calling web service. Please try again later.'));
+        }
     })
 }
+
