dcfce0df0ba4798625c1088ae59c2ad9e95830b7
Sewy44 <76139710+Sewy44@users.noreply.github.com>
2022-02-03 10:20:52 -0500

    added styling and form-validation

--- js/home.js
+++ js/home.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
-
+    $('#search1Div').hide();
+    $('#search2Div').hide();
 });
 
 function compare() {
@@ -10,15 +11,32 @@ function compare() {
     searchSecondUA();
 }
 
+function validate() {
+    var firstUrbanArea = $('#firstUASearch').val();
+    var secondUrbanArea = $('#secondUASearch').val();
+
+    $('.error').remove();
+    if (firstUrbanArea.length < 1) {
+        $('#firstUALabel').after('<span class="error">  This field is required.</span>')
+    }
+    else if (secondUrbanArea.length < 1) {
+        $('#secondUALabel').after('<span class="error">  This field is required.</span>')
+    }
+    else {
+        compare();
+    }
+}
+
 function searchFirstUA() {
 
-    var firstSearchParam = $('#firstUASearch').val();
+    var firstSearchParam = $('#firstUASearch').val().toLowerCase();
     var searchOne = $('#search1Table');
 
     $.ajax({
         type: "GET",
         url: "https://api.teleport.org/api/urban_areas/slug:" + firstSearchParam + "/scores/",
         success: function (dataArray) {
+            $('#search1Div').show();
             var content = '<tr><td>' + dataArray.categories[1].name + '</td><td>' + dataArray.categories[1].score_out_of_10.toFixed(2) + '</td></tr>' +
                 '<tr><td>' + dataArray.categories[7].name + '</td><td>' + dataArray.categories[7].score_out_of_10.toFixed(2) + '</td></tr>' +
                 '<tr><td>' + dataArray.categories[8].name + '</td><td>' + dataArray.categories[8].score_out_of_10.toFixed(2) + '</td></tr>' +
@@ -36,20 +54,21 @@ function searchFirstUA() {
 }
 
 function searchSecondUA() {
-    var firstSearchParam = $('#secondUASearch').val();
-    var searchOne = $('#search2Table');
+    var secondSearchParam = $('#secondUASearch').val().toLowerCase();
+    var searchTwo = $('#search2Table');
 
     $.ajax({
         type: "GET",
-        url: "https://api.teleport.org/api/urban_areas/slug:" + firstSearchParam + "/scores/",
+        url: "https://api.teleport.org/api/urban_areas/slug:" + secondSearchParam + "/scores/",
         success: function (dataArray) {
+            $('#search2Div').show();
             var content = '<tr><td>' + dataArray.categories[1].name + '</td><td>' + dataArray.categories[1].score_out_of_10.toFixed(2) + '</td></tr>' +
                 '<tr><td>' + dataArray.categories[7].name + '</td><td>' + dataArray.categories[7].score_out_of_10.toFixed(2) + '</td></tr>' +
                 '<tr><td>' + dataArray.categories[8].name + '</td><td>' + dataArray.categories[8].score_out_of_10.toFixed(2) + '</td></tr>' +
                 '<tr><td>' + dataArray.categories[10].name + '</td><td>' + dataArray.categories[10].score_out_of_10.toFixed(2) + '</td></tr>' +
                 '<tr><td>' + dataArray.categories[15].name + '</td><td>' + dataArray.categories[15].score_out_of_10.toFixed(2) + '</td></tr>'
 
-            searchOne.append(content);
+            searchTwo.append(content);
         }, error: function () {
             $('#errorMessages')
                 .append($('<li>')
